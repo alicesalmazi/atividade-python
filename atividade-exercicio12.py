9ad7ea48829164e942b10910f8f2ceff0716d879
Alice Salmazi <84449615+alicesalmazi@users.noreply.github.com>
2025-02-28 14:26:56 -0300

    Add files via upload

---
+++ atividade-exercicio12.py
@@ -0,0 +1,6 @@
+#LISTA DE EXERCÍCICIOS – ENTRADAS E SAÍDAS
+#12. Peça ao usuário para digitar sua cidade e imprima a mensagem "Bem-vindo(a) à cidade de [cidade]".
+
+cidade = input("Digite o nome de sua cidade:")
+
+print(f"Bem-vindo(a) à cidade de {cidade}!")
