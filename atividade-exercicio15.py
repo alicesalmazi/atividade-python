5ca80dc4b957151d69d697d9a708463c67610ac3
Alice Salmazi <84449615+alicesalmazi@users.noreply.github.com>
2025-02-28 14:51:03 -0300

    Add files via upload

---
+++ atividade-exercicio15.py
@@ -0,0 +1,9 @@
+#LISTA DE EXERCÍCICIOS – ENTRADAS E SAÍDAS
+#15. Solicite dois números e imprima o resultado da potência do primeiro pelo segundo.
+
+numero1 = int(input("Insira o primeiro número: "))
+numero2 = int(input("Insira o segundo número: "))
+
+potenciaNumerosInseridos = pow(numero1, numero2)
+
+print("Resultado da potenciação:", potenciaNumerosInseridos)
