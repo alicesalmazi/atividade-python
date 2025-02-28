b6bf4b1082f5d452c495f28137f9d10a38aad850
Alice Salmazi <84449615+alicesalmazi@users.noreply.github.com>
2025-02-28 14:09:48 -0300

    Add files via upload

---
+++ atividade-exercicio10.py
@@ -0,0 +1,9 @@
+#LISTA DE EXERCÍCICIOS – ENTRADAS E SAÍDAS
+#10. Peça dois números ao usuário e imprima o resultado da divisão do primeiro pelo segundo.
+
+numero1 = float(input("Insira o primeiro número: "))
+numero2 = float(input("Insira o segundo número: "))
+
+divisaoNumeros = numero1 / numero2
+
+print("Resultado da divisão:", divisaoNumeros)
