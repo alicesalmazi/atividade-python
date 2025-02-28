fdceb3f64627b968d55d44d53052e6768f2bdae2
Alice Salmazi <84449615+alicesalmazi@users.noreply.github.com>
2025-02-28 14:46:41 -0300

    Add files via upload

---
+++ atividade-exercicio13.py
@@ -0,0 +1,8 @@
+#LISTA DE EXERCÍCICIOS – ENTRADAS E SAÍDAS
+#13. Solicite o valor do lado de um quadrado e imprima sua área.
+
+ladoQuadrado = float(input("Insira o tamanho da lateral do quadrado:"))
+
+areaQuadrado = round(pow(ladoQuadrado, 2))
+
+print("O quadrado tem área:", areaQuadrado)
